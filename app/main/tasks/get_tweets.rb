93675239c5cb640d246938a15bec1f3362e86994
Max Plomer <maxplomer@gmail.com>
2015-07-05 21:18:32 -0400

    go back to using take(20)

--- app/main/tasks/get_tweets.rb
+++ app/main/tasks/get_tweets.rb
@@ -8,7 +8,8 @@ class TwitterTasks < Volt::Task
       config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
     end
 
-    client.search("from:", result_type: "recent", count: 20).each do |tweet|
+    client.search("from:", result_type: "recent").take(20).each do |tweet|
+      puts tweet.inspect
       tweets << {text: tweet.text}
     end
 
